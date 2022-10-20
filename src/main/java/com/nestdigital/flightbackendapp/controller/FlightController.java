8fa78c84a9ac05221c1e0a4ed4b8d4ef75aff278
Varshini <varshuk01@gmail.com>
2022-10-20 13:29:42 +0530

    search

--- src/main/java/com/nestdigital/flightbackendapp/controller/FlightController.java
+++ src/main/java/com/nestdigital/flightbackendapp/controller/FlightController.java
@@ -3,6 +3,10 @@ package com.nestdigital.flightbackendapp.controller;
 import com.nestdigital.flightbackendapp.dao.FlightDao;
 import com.nestdigital.flightbackendapp.model.FlightModel;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -22,4 +26,18 @@ public class FlightController {
     public List<FlightModel> viewflight(){
         return (List<FlightModel>) dao.findAll();
     }
+
+    @CrossOrigin(origins = "*")
+    @PostMapping("/deleteflight")
+    public String deleteflight(@RequestBody FlightModel flight)
+    {
+        dao.deleteById(flight.getId());
+        return "{status:'success'}";
+    }
+ @CrossOrigin(origins = "*")
+    @PostMapping("/serachflight")
+    public List<FlightModel> searchflight(@RequestBody FlightModel flight){
+        return (List<FlightModel>) dao.searchFlight(flight.getName());
+ }
+
 }
